418ce593d497fb215f55b4d961fb43032bafbede
Eveliusz <barathino@gmail.com>
2024-10-28 21:37:45 +0100

    poprawki 2 multi

--- projekt-niduc/src/main/java/org/example/Multiplication.java
+++ projekt-niduc/src/main/java/org/example/Multiplication.java
@@ -4,17 +4,15 @@ public class Multiplication {
     public Signal multiplication (Signal first, Signal second){
         if(!first.type.equals("element")){
             if(first.type.equals("decimal")){
-                //zmien decimal->element
+                //zmien decimal->vector
                 decimalToVector(first);
-                vectorToElement(first);
-            }else /*zmien vector->element*/vectorToElement(first);
+            }/*zmien vector->element*/vectorToElement(first);
         }
         if(!second.type.equals("element")){
             if(second.type.equals("decimal")){
-                //zmien decimal->element
+                //zmien decimal->vector
                 decimalToVector(second);
-                vectorToElement(second);
-            }else /*zmien vector->element*/vectorToElement(second);
+            }/*zmien vector->element*/vectorToElement(second);
         }
         return new Signal(
                 String.valueOf((Integer.parseInt(first.value) + Integer.parseInt(second.value)) % 31),
