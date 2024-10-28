d65d0ba35e3c3ffd3e374cd0e7bfaa4bb239b3c8
Marta <103411042+muffinax@users.noreply.github.com>
2024-10-28 15:59:27 +0100

    wielomian generujący i przykładowe dane

---
+++ projekt-niduc/src/main/java/org/example/Generator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.IOError;
+
+public class Generator{
+    //marta - obliczanie wielomianu generującgo - zrobię z tego osobną klasę
+    int[] generating_polynomial(int[] signal, int t){
+        int[] generator={0,1};
+        if(t<1)
+            throw new IllegalArgumentException("zdolność korekcyjna musi być większa niż 1");
+
+        for(int i=2;i<2*t+1;i++){
+            int[] podstawienie = {0,i};
+            //generator = //multyplikacja(generator,podstawienie)
+        }
+        return generator;
+    }
+}
