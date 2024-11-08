a455cbc3ee13302d06b336a35e9cdf30d822c6d4
nraea <przytockan@gmail.com>
2024-11-08 11:26:10 +0100

    nieprzetestowany koder

---
+++ projekt-niduc/src/main/java/org/example/Encoder.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Encoder {
+    public String[] encode(Polynomial message){
+        Generator generator = new Generator();
+        Addition addition = new Addition();
+        Multiplication multiplication = new Multiplication();
+        String[] encodedMessage = new String[message.getPolynomial().length];
+        String[] rx = new String[message.getPolynomial().length];
+        Polynomial xnkxm = new Polynomial(message);
+        Signal zero = new Signal();
+        //zrobienie wielomianu x^(n-k) * m(x)
+        for(int i = 0; i < xnkxm.getPolynomial().length; i++){
+            for(int j = 0; j < 12; j++){
+                xnkxm.getPolynomialSignal(i).setValue(xnkxm.getPolynomialSignal(i).getValueD() + "0", "decimal");
+            }
+        }
+        //uzyskanie r(x)
+        Polynomial mxg = multiplication.mul_polynomials(xnkxm, generator.polynomial_generator());
+        for(int i = 0; i < message.getPolynomial().length;i++){
+            rx[i] = mxg.getPolynomialSignal(i).getValueD();
+            //dopełnienie r(x) do odpowiedniej długosci
+            if(mxg.getPolynomialSignal(i).getValueD().length() < 12)
+                while(rx[i].length() < 12){
+                    rx[i] = "0" + rx[i];
+                }
+        }
+        //dodanie rozszerzonego do odpowiedniej dlugosci r(x)
+        for(int i = 0; i < message.getPolynomial().length;i++){
+            encodedMessage[i] = message.getPolynomialSignal(i).getValueD() + rx[i];
+        }
+        return encodedMessage;
+    }
+}
