9bbc39545cac633ea4ec3b77180ad130c543f984
nraea <przytockan@gmail.com>
2024-11-08 12:30:39 +0100

    niedziałający koder (nie działa for w linii 15 w klasie encoder)

--- projekt-niduc/src/main/java/org/example/Encoder.java
+++ projekt-niduc/src/main/java/org/example/Encoder.java
@@ -6,26 +6,35 @@ public class Encoder {
         Multiplication multiplication = new Multiplication();
         String[] encodedMessage = new String[message.getPolynomial().length];
         String[] rx = new String[message.getPolynomial().length];
+        //wielomian x^(n-k) * m(x)
         Polynomial xnkxm = new Polynomial(message);
         //zrobienie wielomianu x^(n-k) * m(x)
-        for(int i = 0; i < xnkxm.getPolynomial().length; i++){
-            for(int j = 0; j < 12; j++){
+        System.out.println("Wielomian m(x) na wielomian x^(n-k) * m(x):");
+        for(int i = 0; i < message.getPolynomial().length; i++){
+            System.out.println(xnkxm.getPolynomialSignal(i));
+            for(int j = 0; j < 12; j++){ //n-k = 12 //ten for nie działą
                 xnkxm.getPolynomialSignal(i).setValue(xnkxm.getPolynomialSignal(i).getValueD() + "0", "decimal");
             }
+            System.out.println(xnkxm.getPolynomialSignal(i).getValueD() + "\n");
         }
         //uzyskanie r(x)
+        System.out.println("\nReszty r(x):");
         Polynomial mxg = multiplication.mul_polynomials(xnkxm, generator.polynomial_generator());
         for(int i = 0; i < message.getPolynomial().length;i++){
             rx[i] = mxg.getPolynomialSignal(i).getValueD();
             //dopełnienie r(x) do odpowiedniej długosci
-            if(mxg.getPolynomialSignal(i).getValueD().length() < 12)
+            if(mxg.getPolynomialSignal(i).getValueD().length() < 12){
                 while(rx[i].length() < 12){
                     rx[i] = "0" + rx[i];
                 }
+            }
+            System.out.println(rx[i] + "\n");
         }
         //dodanie rozszerzonego do odpowiedniej dlugosci r(x)
+        System.out.println("Zakodowane c(x):");
         for(int i = 0; i < message.getPolynomial().length;i++){
             encodedMessage[i] = message.getPolynomialSignal(i).getValueD() + rx[i];
+            System.out.println(encodedMessage[i] + "\n");
         }
         return encodedMessage;
     }
--- projekt-niduc/src/main/java/org/example/Main.java
+++ projekt-niduc/src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
         Multiplication multi=new Multiplication();
         Addition addi=new Addition();
+        Encoder encoder = new Encoder();
         Polynomial gen_test = new Polynomial(new String[] {"A16","A09","A13","A03","A10","A25","A13","A02","A10","A00","A22","A06","A00"}, "element");
         Polynomial poly1 = new Polynomial(new String[] {"A12","A00"}, "element");
         Polynomial poly2 = new Polynomial(new String[] {"A26","A10","A15","A28","A00"}, "element");
@@ -25,16 +26,21 @@ public class Main {
 //        System.out.println(addi.addition(first,second).getValueE());
 //        addi.add_polynomials(poly1,poly2).show_polynomial();
 //        System.out.println();
-        multi.mul_polynomials(poly1,poly2).show_polynomial();
-        System.out.println();
-        generator_poly= generator.polynomial_generator();
-        generator_poly.show_polynomial();
-        System.out.println();
+        //multi.mul_polynomials(poly1,poly2).show_polynomial();
+        //System.out.println();
+        //generator_poly= generator.polynomial_generator();
+        //generator_poly.show_polynomial();
+        //System.out.println();
 
         //tudidu
 //        polynomial.add_polynomials(poly2.getPolynomial());
 //        polynomial.show_polynomial();
 //        poly1.mul_polynomials(poly2.getPolynomial());
 //        poly1.show_polynomial();
+
+        String[] poly1coded = encoder.encode(poly1);
+        //for(int i = 0; i < poly1coded.length; i++){
+        //    System.out.println(poly1coded[i]);
+        //}
     }
 }
